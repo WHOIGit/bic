d8cec9b5434b03a6d9bb9aad13388335765bc57c
Joe Futrelle <joefutrelle@gmail.com>
2014-01-08 19:21:14 +0000

    save/load for Lightmap

--- trunk/opencv/lib/Lightfield.hpp
+++ trunk/opencv/lib/Lightfield.hpp
@@ -2,6 +2,9 @@
 #include <exception>
 #include <opencv2/opencv.hpp>
 
+#include <iostream>
+// FIXME remove iostream
+
 using namespace cv;
 using namespace std;
 
@@ -11,9 +14,11 @@ class Lightfield {
   Mat alpha_ones;
   void init(Mat image) {
     if(sum.empty()) {
-      sum = Mat::zeros(image.rows, image.cols, CV_32F);
-      count = Mat::zeros(image.rows, image.cols, CV_32F);
-      alpha_ones = Mat::ones(image.rows, image.cols, CV_32F);
+      int h = image.size().height;
+      int w = image.size().width;
+      sum = Mat::zeros(h, w, CV_32F);
+      count = Mat::zeros(h, w, CV_32F);
+      alpha_ones = Mat::ones(h, w, CV_32F);
     }
   }
 public:
@@ -21,10 +26,10 @@ public:
   void addImage(Mat image, Mat alpha) {
     // initialize sum/count images
     init(image);
-    assert(image.rows == sum.rows);
-    assert(image.cols == sum.cols);
-    assert(image.rows == alpha.rows);
-    assert(image.cols == alpha.cols);
+    assert(image.size().height == sum.size().height);
+    assert(image.size().width == sum.size().width);
+    assert(image.size().height == alpha.size().height);
+    assert(image.size().width == alpha.size().width);
     Mat image32f;
     image.convertTo(image32f, CV_32F);
     sum += image32f.mul(alpha);
@@ -39,16 +44,44 @@ public:
     assert(!sum.empty());
     return sum / count;
   }
-  // FIXME add save/load
   Mat correct(Mat image) {
     Mat average = getAverage();
-    assert(image.rows == average.rows);
-    assert(image.cols == average.cols);
+    assert(image.size().height == average.size().height);
+    assert(image.size().width == average.size().width);
     double minLightmap, maxLightmap;
     minMaxLoc(average, &minLightmap, &maxLightmap);
     // perform correction
     Mat image32f;
     image.convertTo(image32f, CV_32F);
     return (image32f / average) * (maxLightmap - minLightmap);
+  }
+  void save(string pathname) {
+    // FIXME assert that the path ends ".tif" or equiv
+    Mat average = getAverage();
+    Mat composite;
+    int h = average.size().height;
+    int w = average.size().width;
+    composite.create(h * 2, w, CV_32F);
+    Mat top(composite, Rect(0, 0, w, h));
+    Mat bottom(composite, Rect(0, h, w, h));
+    average.copyTo(top);
+    count.copyTo(bottom);
+    Mat composite_16u;
+    composite.convertTo(composite_16u, CV_16U);
+    imwrite(pathname, composite_16u);
+  }
+  void load(string pathname) {
+    Mat composite_16u = imread(pathname, CV_LOAD_IMAGE_ANYDEPTH);
+    assert(composite_16u.type() == CV_16U);
+    int h = composite_16u.size().height / 2;
+    int w = composite_16u.size().width;
+    Mat composite;
+    composite_16u.convertTo(composite, CV_32F);
+    Mat top(composite, Rect(0, 0, w, h));
+    Mat bottom(composite, Rect(0, h, w, h));
+    init(top); // initialize accumulation matricies
+    top.copyTo(sum);
+    bottom.copyTo(count);
+    sum = sum.mul(count);
   }
 };
--- trunk/opencv/lib/main.cpp
+++ trunk/opencv/lib/main.cpp
@@ -15,27 +15,32 @@ int main(int argc, char** argv) {
   ifstream inpaths(PATH_FILE);
   string path;
   Lightfield model;
-  while(getline(inpaths,path)) {
-    cout << "Learning " << path << endl;
-    Mat cfa_LR = imread(path, CV_LOAD_IMAGE_ANYDEPTH);
-    model.addImage(cfa_LR);
-  }
-  int count = 0;
-  ifstream inpaths2(PATH_FILE);
-  while(getline(inpaths2,path)) {
-    cout << "Correcting " << path << endl;
-    Mat cfa_LR = model.correct(imread(path, CV_LOAD_IMAGE_ANYDEPTH));
-    cout << "Demosaicing " << path << endl;
-    Mat rgb_LR = demosaic_hqlinear(cfa_LR,"rggb");
-    stringstream outpaths;
-    string outpath;
-    outpaths << "out/correct" << count << ".tiff";
-    outpath = outpaths.str();
-    cout << "Saving RGB to " << outpath << endl;
-    Mat rgb_LR_16u;
-    rgb_LR.convertTo(rgb_LR_16u, CV_16U);
-    imwrite(outpath, rgb_LR_16u);
-    count++;
+  if(string(argv[1]) == "learn") {
+    while(getline(inpaths,path)) {
+      cout << "Learning " << path << endl;
+      Mat cfa_LR = imread(path, CV_LOAD_IMAGE_ANYDEPTH);
+      model.addImage(cfa_LR);
+    }
+    model.save("model.tiff");
+  } else if(string(argv[1]) == "correct") {
+    model.load("model.tiff");
+    int count = 0;
+    ifstream inpaths2(PATH_FILE);
+    while(getline(inpaths2,path)) {
+      cout << "Correcting " << path << endl;
+      Mat cfa_LR = model.correct(imread(path, CV_LOAD_IMAGE_ANYDEPTH));
+      cout << "Demosaicing " << path << endl;
+      Mat rgb_LR = demosaic_hqlinear(cfa_LR,"rggb");
+      stringstream outpaths;
+      string outpath;
+      outpaths << "out/correct" << count << ".tiff";
+      outpath = outpaths.str();
+      cout << "Saving RGB to " << outpath << endl;
+      Mat rgb_LR_16u;
+      rgb_LR.convertTo(rgb_LR_16u, CV_16U);
+      imwrite(outpath, rgb_LR_16u);
+      count++;
+    }
   }
   return 0;
 }
