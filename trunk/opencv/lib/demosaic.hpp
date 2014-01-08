1ccdc569a45a05994ddd65d82f2462e2902dff32
Joe Futrelle <joefutrelle@gmail.com>
2014-01-08 20:28:16 +0000

    removed evil global using namespace

--- trunk/opencv/lib/demosaic.hpp
+++ trunk/opencv/lib/demosaic.hpp
@@ -1,6 +1,4 @@
 #pragma once
 #include <opencv2/opencv.hpp>
 
-using namespace cv;
-
-Mat demosaic_hqlinear(Mat cfa, string cfaPattern="rggb");
+cv::Mat demosaic_hqlinear(cv::Mat cfa, std::string cfaPattern="rggb");
