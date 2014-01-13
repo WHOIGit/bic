f36d20a201e6258616a912e392849c4146808da4
Joe Futrelle <joefutrelle@gmail.com>
2014-01-13 19:37:26 +0000

    general-purpose interpolation and binning support. simple linear binning first implementation.

---
+++ trunk/opencv/lib/interpolation.hpp
@@ -0,0 +1,28 @@
+#pragma once
+#include <cmath>
+
+template <typename T> class LinearBinning {
+private:
+  std::vector<T> bins;
+public:
+  LinearBinning(T low, T high, T width) {
+    for(T v = low; v <= high; v += width) {
+      bins.push_back(v);
+    }
+  }
+  std::vector<T> getBins() {
+    return bins;
+  }
+  std::vector<std::pair<T,double> > interpolate(T value) {
+    std::vector<std::pair<T,double> > result;
+    typename std::vector<T>::iterator it = bins.begin();
+    double binWidth = *(it+1) - *it;
+    for(; it != bins.end(); ++it) {
+      T diff = std::abs(*it - value);
+      if(diff < binWidth) {
+	result.push_back(std::pair<T,double>(*it, 1.0 - (diff / binWidth)));
+      }
+    }
+    return result;
+  }
+};
