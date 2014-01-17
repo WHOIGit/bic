bd06764e1d169acaf907d9832fe1a91d4438e6a9
Joe Futrelle <joefutrelle@gmail.com>
2014-01-17 15:12:25 +0000

    more hacking on stereo alignment. this maybe works? need to test with V4 imagery and compare with Python results

--- trunk/opencv/lib/main.cpp
+++ trunk/opencv/lib/main.cpp
@@ -9,9 +9,8 @@
 
 using namespace std;
 
-void doit() {
+void doit(string fname) {
   using namespace cv;
-  string fname="stereocam.png";
   Mat y_LR_in = imread(fname);
   Mat y_LR;
   y_LR_in.convertTo(y_LR, CV_32F);
@@ -25,47 +24,49 @@ void doit() {
   int ts = 64; // template size
   int ts2 = ts / 2;
   // perform template matching
-  Mat out = Mat::zeros(h,w2,CV_32F);
+  Mat out = Mat::zeros(h,w2+ts+1,CV_32F);
   int oxen[3] = { 0, ts, 0-ts };
-  for(int oxi = 0; oxi < 3; oxi++) {
-    int ox = oxen[oxi];
-    for(int y = 0; y < h-(ts*2); y += ts+1) {
+  for(int y = 0; y < h-(ts*2); y += ts+1) {
+    for(int oxi = 0; oxi < 3; oxi++) {
+      int ox = oxen[oxi];
       // select the center pixels of the left image
       Mat templ = Mat(y_LR, Rect((w4+ox)-ts2,y,ts,ts));
       // now match that template against the corresponding horizontal
       // strip of right image and accumulate into and output "strips"
       // image
       // image is w2 x ts*2. template is ts x ts
-      // outstrip is w2-ts+1 x ts+1
+      // outstrip is w2+1 x ts+1
       Mat instrip = Mat(y_LR, Rect(w2,y,w2,ts*2));
       Mat outstrip = Mat::zeros(ts+1, w2-ts+1, CV_32F);
-      Mat roi = Mat(out, Rect(0,y,w2-ts+1,ts+1));
-      matchTemplate(instrip, templ, outstrip, CV_TM_CCORR);
+      matchTemplate(instrip, templ, outstrip, CV_TM_SQDIFF_NORMED);
+      Mat roi = Mat(out, Rect(ts+ox,y,w2-ts+1,ts+1));
       roi += outstrip;
     }
+    // now normalize this strip
+    //Mat roi = Mat(out, Rect(ts,y,w2-ts,ts+1));
+    //normalize(roi,roi,0,1,NORM_MINMAX);
   }
-  double minVal, maxVal;
-  Point minLoc, maxLoc;
-  minMaxLoc(out, &minVal, &maxVal, &minLoc, &maxLoc);
-  cout << "min/max of out is " << minVal << "," << maxVal << endl;
-  out /= maxVal;
-  out *= 255;
+  Mat clipped(out, Rect(ts*2,0,w2-(ts*3),h));
+  normalize(clipped,clipped,1,0,NORM_MINMAX);
+  clipped *= 255;
   Mat out8u;
-  out.convertTo(out8u, CV_8U);
+  clipped.convertTo(out8u, CV_8U);
   out /= 255;
   imwrite("strips.tiff",out8u);
   // sum all rows of out image
   Mat summed;
   reduce(out, summed, 0, CV_REDUCE_SUM, CV_32F);
   // now find the x location of maximum of this sum
+  Point minLoc, maxLoc;
+  double minVal, maxVal;
   minMaxLoc(summed, &minVal, &maxVal, &minLoc, &maxLoc);
-  int max_x = maxLoc.x - ts2;
+  int max_x = (maxLoc.x - ts2)/2;
   int dx = w4 - max_x;
   cout << "found match at " << max_x << ", offset = " << dx << endl;
 }
 
 int main(int argc, char **argv) {
-  doit();
+  doit(argv[1]);
   /*
   if(string(argv[1])=="learn") {
     learn_prototype();
