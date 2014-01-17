f4def0e03470a181d096e212c4f742c43fe86627
Joe Futrelle <joefutrelle@gmail.com>
2014-01-17 18:54:22 +0000

    this is a hack but I'm finally getting matches so I can build on this

--- trunk/opencv/lib/main.cpp
+++ trunk/opencv/lib/main.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <time.h>
 #include <opencv2/opencv.hpp>
+#include <boost/tokenizer.hpp>
 #include "prototype.hpp"
 #include "interpolation.hpp"
 #include "illumination.hpp"
@@ -10,7 +11,7 @@
 
 using namespace std;
 
-void doit(cv::Mat y_LR_in) {
+int doit(cv::Mat y_LR_in) {
   using namespace cv;
   Mat y_LR;
   y_LR_in.convertTo(y_LR, CV_32F);
@@ -26,10 +27,11 @@ void doit(cv::Mat y_LR_in) {
   // now select a random template location in the left image
   uint64 seed = time(NULL);
   RNG rng(seed);
+  /*
   Mat all(ts, w2-ts+1, CV_32F);
   Mat out(ts, w2-ts+1, CV_32F);
-  for(int i = 0; i < 100; i++) {
-    int x = rng.uniform(0,w2-ts*2);
+  for(int i = 0; i < 5; i++) {
+    int x = rng.uniform(ts*2,w2-ts*4);
     int y = rng.uniform(0,h-ts*2+1);
     // match the template against the right image
     Mat right(y_LR,Rect(w2,y,w2,ts*2-1));
@@ -46,9 +48,33 @@ void doit(cv::Mat y_LR_in) {
     allR += outL;
   }
   normalize(all,all,0,255,NORM_MINMAX);
+  Point minLoc, maxLoc;
+  double minVal, maxVal;
+  minMaxLoc(all,&minVal,&maxVal,&minLoc,&maxLoc);
+  int max_x = maxLoc.x - ts;
   Mat out8u;
   all.convertTo(out8u,CV_8U);
   imwrite("match.tiff",out8u);
+  return max_x;
+  */
+  int x = rng.uniform(ts*2,w2-ts*4);
+  int y = rng.uniform(0,h-ts*2+1);
+  Mat templ(y_LR,Rect(x,y,ts,ts));
+  Mat right(y_LR,Rect(w2,0,w2,h-ts));
+  Mat out;
+  matchTemplate(right, templ, out, CV_TM_CCOEFF_NORMED);
+  normalize(out,out,0,1,NORM_MINMAX);
+  imwrite("match.tiff",out*255);
+  Point minLoc, maxLoc;
+  double minVal, maxVal;
+  minMaxLoc(out,&minVal,&maxVal,&minLoc,&maxLoc);
+  int max_x = maxLoc.x;
+  int max_y = maxLoc.y;
+  int mx = w2+max_x;
+  int my = max_y;
+  rectangle(y_LR_in,Point(x,y),Point(x+ts,y+ts),0);
+  rectangle(y_LR_in,Point(mx,my),Point(mx+ts,my+ts),0);
+  imwrite("pic.tiff",y_LR_in);
 }
 
 cv::Mat get_green(cv::Mat cfa_LR) {
@@ -60,11 +86,28 @@ cv::Mat get_green(cv::Mat cfa_LR) {
 
 int main(int argc, char **argv) {
   using namespace cv;
+  /*
   Mat y_LR = imread(argv[1], CV_LOAD_IMAGE_ANYDEPTH);
-  cout << y_LR.size().width << "," << y_LR.size().height << endl;
   Mat green = get_green(y_LR);
-  cout << green.size().width << "," << green.size().height << endl;
-  doit(green);
+  int x = doit(green);
+  cout << argv[1] << "," << x << endl;
+  */
+  ifstream inpaths(argv[1]);
+  typedef boost::tokenizer< boost::escaped_list_separator<char> > Tokenizer;
+  vector<string> fields;
+  string line;
+  while(getline(inpaths,line)) { // read pathames from a file
+    Tokenizer tok(line);
+    fields.assign(tok.begin(),tok.end());
+    string inpath = fields.front();
+    int offset = (int)(atoi(fields.back().c_str()));
+    Mat y_LR = imread(inpath, CV_LOAD_IMAGE_ANYDEPTH);
+    Mat green = get_green(y_LR);
+    imwrite("green.tiff",green);
+    int x = doit(green);
+    cout << inpath << "," << offset << "," << x << endl;
+    break;
+  }
   /*
   if(string(argv[1])=="learn") {
     learn_prototype();
