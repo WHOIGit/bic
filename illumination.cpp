afd022f771e8cbfc92036903b903fa1f28a62e34
Joe Futrelle <joefutrelle@gmail.com>
2014-04-14 15:35:11 +0000

    addressing #2821. I'm using a constant factor of 0.33 to scale down the lightmap ratio after correction, and then the min and max brightness parameters are used to scale the result. The factor is dimensionless and so shouldn't result in hugely different results for different cameras/altitudes/lightmaps, and gives "reasonable" results when the min/max brightness levels are set to their new defaults of 0 and 1. will need to test with some other data though; I have some at hand.

--- illumination.cpp
+++ illumination.cpp
@@ -2,12 +2,18 @@
 
 /**
  * Correct an image by dividing it by an illumination model image,
- * e.g., one computed by frame averaging.
+ * e.g., one computed by frame averaging. The corrected image, whose
+ * values in floating-point representation are >1 for every pixel in
+ * the input image that is brighter than the corresponding pixel in
+ * the lightmap, are scaled by 0.33 of the output array's data
+ * type. To avoid clipping and perform your own adjustment, use a
+ * floating-point result type. Supported types are CV_8U, CV_16U,
+ * and CV_32F.
  *
  * @param _src the source image
  * @param _dst the destination image (must be same size and type as src)
  * @param lightfield the appropriate average image returned by the lightfield
- * @return a new, correctly-illuminated image (in floating point)
+ * @return a new, correctly-illuminated image (in the output array's type)
  */
 void illum::correct(cv::InputArray _src, cv::OutputArray _dst, cv::Mat lightfield) {
   using namespace cv;
@@ -16,22 +22,26 @@ void illum::correct(cv::InputArray _src, cv::OutputArray _dst, cv::Mat lightfiel
   Mat dst = _dst.getMat();
   if(src.size() != dst.size())
     throw std::runtime_error("input/output image size mismatch");
-  if(src.type() != dst.type())
-    throw std::runtime_error("input/output image type mismatch");
   if(src.size() != lightfield.size())
     throw std::runtime_error("input image not same size as lightfield");
   Mat src32f;
   src.convertTo(src32f, CV_32F); // convert to floating point
-  // find intensity range of lightfield image
-  double minLightmap, maxLightmap;
-  minMaxLoc(lightfield, &minLightmap, &maxLightmap);
-  // divide image by lightfield, then normalize to intensity range of lightfield
+  // divide image by lightfield
   Mat lightfield32f;
   if(lightfield.type() != CV_32F) {
     lightfield.convertTo(lightfield32f, CV_32F);
   } else {
     lightfield32f = lightfield;
   }
-  Mat correct32f = (src32f / lightfield32f) * (maxLightmap - minLightmap);
-  correct32f.convertTo(dst, src.type());
+  // now compute corrected image in its floating-point representation
+  Mat correct32f = src32f / lightfield32f;
+  // now we need to convert the values to the result type,
+  // including the scaling operation
+  if(dst.depth()==CV_8U)
+    correct32f *= 85;
+  if(dst.depth()==CV_16U)
+    correct32f *= 21843;
+  if(dst.depth()==CV_32F)
+    correct32f *= 0.33;
+  correct32f.convertTo(dst, dst.type());
 }
